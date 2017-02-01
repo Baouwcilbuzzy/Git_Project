a11d3cf1fd3a99598a698ed76b8af82c3307abef
Basil Çapar <bas98c@hotmail.com>
2017-02-01 08:46:37 -0500

    Ajouter  2 commentaires

---
+++ Git_Project/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(­­"Hello World");
+            Console.WriteLine("My name is : ");
+            Console.WriteLine("Choisissez un nombre");
+
+            // Voici un commentaire
+            
+            // Salut
+
+            Random générateurAléatoire = new Random();
+        }
+    }
+}
---
+++ Git_Project/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("Git_Project")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CLG")]
+[assembly: AssemblyProduct("Git_Project")]
+[assembly: AssemblyCopyright("Copyright © CLG 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("08af3259-f85a-4336-ad4c-5be4eb2e16c9")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
